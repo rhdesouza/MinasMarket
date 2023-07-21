f6a168526cb2108ad4b882617685f38cec9b4a5f
Rafael Henrique <rhdesouza@hotmail.com>
2023-07-21 13:28:25 -0300

    Create AnnouncementImageAPITest.java

---
+++ src/test/java/com/minas/market/integration/webapi/contract/AnnouncementImageAPITest.java
@@ -0,0 +1,15 @@
+package com.minas.market.integration.webapi.contract;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class AnnouncementImageAPITest {
+
+    //TODO: Implementar este teste
+    @BeforeEach
+    public void init() throws Exception {
+    }
+}
